1fc5399458bcd8b90e19b7a67c5e55c05f8eb4a9
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-24 16:08:26 +0300

    Created vote page

--- voting_app/views.py
+++ voting_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Voting, Option, Vote
 
-# Create your views here.
+def voting_list(request):
+    votings = Voting.objects.all()
+    return render(request, 'index.html', {'votings': votings})
+
+def voting_detail(request, voting_id):
+    voting = get_object_or_404(Voting, pk=voting_id)
+    options = Option.objects.filter(voting_id=voting)
+
+    if request.method == 'POST':
+        selected_option_id = request.POST.get('option')
+        selected_option = get_object_or_404(Option, pk=selected_option_id)
+        existing_vote = Vote.objects.filter(user=request.user, option__voting_id=voting)
+        Vote.objects.create(user=request.user, option=selected_option)
+
+    return render(request, 'voting.html', {'voting': voting, 'options': options})
