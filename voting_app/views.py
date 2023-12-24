965d3331431109c03f37c43cadbf9403ee11ea7d
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-24 16:09:05 +0300

    Fix bug with double voting

--- voting_app/views.py
+++ voting_app/views.py
@@ -13,6 +13,8 @@ def voting_detail(request, voting_id):
         selected_option_id = request.POST.get('option')
         selected_option = get_object_or_404(Option, pk=selected_option_id)
         existing_vote = Vote.objects.filter(user=request.user, option__voting_id=voting)
+        if existing_vote.exists():
+            return render(request, 'already.html')
         Vote.objects.create(user=request.user, option=selected_option)
 
     return render(request, 'voting.html', {'voting': voting, 'options': options})
