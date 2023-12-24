ac2d1e0b173c50c428f68cb6e2eeeb76a093dbd5
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-24 14:21:08 +0300

    Added voting, option, vote models

--- voting_app/models.py
+++ voting_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Voting(models.Model):
+  title = models.CharField(max_length=255)
+  description = models.TextField()
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Option(models.Model):
+  voting_id = models.ForeignKey(Voting, on_delete=models.CASCADE)
+  content = models.CharField(max_length=255)
+
+
+class Vote(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  option = models.ForeignKey(Option, on_delete=models.CASCADE)
