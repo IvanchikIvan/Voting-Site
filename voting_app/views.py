87c88271b07f87488fd6001f6a811b501583fdc9
Kavalskiy <vanyamalisheff@yandex.ru>
2023-12-30 18:16:32 +0300

    Revamped the system for verifying repeat voting

--- voting_app/views.py
+++ voting_app/views.py
@@ -15,16 +15,16 @@ def voting_list(request):
 def voting_detail(request, voting_id):
     voting = get_object_or_404(Voting, pk=voting_id)
     options = Option.objects.filter(voting_id=voting)
-
+    existing_vote = Vote.objects.filter(user=request.user, option__voting_id=voting)
     if request.method == 'POST':
         selected_option_id = request.POST.get('option')
         selected_option = get_object_or_404(Option, pk=selected_option_id)
-        existing_vote = Vote.objects.filter(user=request.user, option__voting_id=voting)
+        if not existing_vote.exists():
+            Vote.objects.create(user=request.user, option=selected_option)
         if existing_vote.exists():
-            return render(request, 'already.html')
-        Vote.objects.create(user=request.user, option=selected_option)
-
-    return render(request, 'voting.html', {'voting': voting, 'options': options})
+            return render(request, 'voting.html', {'voting': voting, 'options': options, 'already_voted': 1})
+    else:
+        return render(request, 'voting.html', {'voting': voting, 'options': options, 'already_voted': 0})
 
 
 def user_login(request):
