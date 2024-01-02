8274f60f2f61916db6864827a1a7309de22acab0
Kavalskiy <vanyamalisheff@yandex.ru>
2024-01-02 23:43:10 +0300

    fix create voting form

--- voting_app/forms.py
+++ voting_app/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import Voting, Option
 
+
 class VotingForm(forms.ModelForm):
-    options = forms.CharField(widget=forms.TextInput(attrs={'class': 'option-field'}))
     class Meta:
         model = Voting
-        fields = ['title', 'description', 'options']
+        fields = ['title', 'description']
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
@@ -13,11 +13,12 @@ class VotingForm(forms.ModelForm):
 
     def save(self, commit=True):
         voting = super(VotingForm, self).save(commit=False)
-        voting.author = self.request.user
+        if self.request:
+            voting.author = self.request.user
         voting.save()
-        print(self.cleaned_data['options'])
-        options = self.cleaned_data['options'].split('\n')
-        for option_text in options:
+
+        options_input = self.data.getlist('option-field')
+        for option_text in options_input:
             Option.objects.create(voting_id=voting, content=option_text.strip())
 
         return voting
